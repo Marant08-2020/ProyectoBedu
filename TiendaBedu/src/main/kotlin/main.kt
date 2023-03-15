4c8351f40b19a9a6edd37cb048fb5b4a0ce01c1b
mojicamarcumplo <marco.mojica@ umplo.com>
2023-03-14 22:28:20 -0600

    Commit menu inicial se aplica ciclos y declaración de variables

--- TiendaBedu/src/main/kotlin/main.kt
+++ TiendaBedu/src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    print("Hola chicos....")
+    var opc: Int
+    // Menu de la aplicación
+    do {
+        println("1. Crear Producto")
+        println("2. Crear Orden")
+        println("3. Crear Venta")
+        println("4. Salier")
+        print("Ingrese una opción: ")
+        opc = readLine()?.toInt() as Int
+
+    } while (opc != 4)
+
 }
