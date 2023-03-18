2025a03693aab9148b3d9a7926475e9735e69099
mojicamarcumplo <marco.mojica@ umplo.com>
2023-03-17 23:53:11 -0600

    Menu de usuario que administra a los usarios

---
+++ TiendaBedu/src/main/kotlin/menus/menuUsuarios.kt
@@ -0,0 +1,104 @@
+package menus
+
+import user.User
+import java.util.LinkedList
+// importación temporal en lo que se creal el menu proncipal
+import menuPrincipa
+
+
+fun inicioSesion(){
+    var opc: Int
+
+    do {
+        println("1. Iniciar sesión")
+        println("2. Crear user")
+        println("3. Cambiar pasword")
+        println("4. Salir")
+        print("Ingrese una opción: ")
+        opc = readLine()?.toInt() as Int
+
+        when (opc) {
+            1 -> iniciarSesion()
+            2 -> crearUser()
+            3 -> cambiarPassword()
+            4 -> println("Saliendo del menu")
+            else -> {
+                println("Opción no válida")
+            }
+        }
+
+    } while (opc != 4)
+
+}
+// Variable global de usuarios
+var arregloUser =  LinkedList<User>()
+
+// Funcion que instancia y crear usuarios
+fun crearUser(){
+
+    print("Nombre:")
+    val name =readln()
+    print("Apellido:")
+    val apellido =readln()
+    print("Email:")
+    val email =readln()
+    print("Contraseña:")
+    val password =readln()
+
+    val usuario = User(nombre = name,
+        apellido = apellido, email = email,
+        password = password)
+    println("Se agredo el userio con id:${usuario.id}")
+
+    arregloUser.addLast(usuario)
+
+
+
+}
+// Función que inicia sesión usando el id del usuario
+fun iniciarSesion() {
+    if(arregloUser.isEmpty()){
+        println("**Por favor agregar usuarios**")
+    }else{
+        print("UserID:")
+        val userId =readln().toInt()
+        print("Constraseña:")
+        val passwordUser =readln()
+        // Funcion lambda
+        val loginUser = arregloUser.filter { User-> User.id == userId }
+        if (loginUser.isEmpty()){
+            println("**El usuario no existe**")
+        }else{
+            if( loginUser[0].password == passwordUser){
+                menuPrincipa()
+            }else{
+                println("***Contraseña inválidad***")
+            }
+        }
+
+    }
+}
+
+fun cambiarPassword(){
+    print("UserID:")
+    val userId =readln().toInt()
+    val userAmodificar = arregloUser.filter { User-> User.id == userId }
+    if(userAmodificar.isEmpty()){
+        println("***Usuario no encontrado***")
+
+    }else{
+        print("contraseña nueva:")
+        var nuevoPassword =readln()
+        print("Con firmar contraseña nueva:")
+        var confirmarPassword =readln()
+        if(nuevoPassword== confirmarPassword){
+            // usa el set de la class data
+            userAmodificar[0].password = nuevoPassword
+            println("***Contraseña modificada***")
+        }else{
+            println("***Validar contraseña***")
+        }
+
+    }
+
+}
