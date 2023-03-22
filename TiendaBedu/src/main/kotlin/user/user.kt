dc3ae62dee197f9f90e8acb7cc3a1e64a3634996
mojicamarcumplo <marco.mojica@ umplo.com>
2023-03-22 13:06:22 -0600

    Variables estática para el control de los id.

--- TiendaBedu/src/main/kotlin/user/user.kt
+++ TiendaBedu/src/main/kotlin/user/user.kt
@@ -1,11 +1,15 @@
 package user
 
-private var contadorUser: Int =0
+//private var contadorUser: Int =0
 data class User(var id: Int =0,
                 var nombre: String,
                 var apellido: String,
                 var email: String,
                 var password: String){
+    // Variable estática
+    companion object{
+        private var contadorUser: Int =0
+    }
     init {
         contadorUser += 1
 
