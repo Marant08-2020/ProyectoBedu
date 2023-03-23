bee9bdfde4a10016186f216e133dd9fdd7ba1755
Alan Sandoval <asandoval@optimen.com.mx>
2023-03-23 12:10:26 -0600

    interfaz de impuesto agregada

--- TiendaBedu/src/main/kotlin/orden/orden.kt
+++ TiendaBedu/src/main/kotlin/orden/orden.kt
@@ -1,4 +1,15 @@
 package orden
 
-class orden {
+import Impuesto.Impuesto
+
+class orden: Impuesto {
+
+    override var ivaTax: Float = 1.16f
+//        set(value) {}
+    override fun calcularImpuesto(costo: Float): Float {
+        ivaTax = 1.13f
+        return super.calcularImpuesto(costo)
+    }
+    val total = calcularImpuesto(13.5f)
+
 }
